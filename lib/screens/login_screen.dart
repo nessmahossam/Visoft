d28f7bbaf0cbb3be3ea3c71bd12464cf97a3ca6a
Nessma Hossam <NTG@Agehad.ntgclarity.com>
2021-05-22 21:22:18 +0200

    HomePage-Categories

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,158 +1,135 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:viisoft/constants.dart';
-import 'package:viisoft/models/category_model.dart';
-import 'package:viisoft/widgets/category_section.dart';
-import 'package:viisoft/widgets/projectCard.dart';
+import 'package:viisoft/screens/home_screen.dart';
+import 'package:viisoft/screens/mainScreen.dart';
+import 'package:viisoft/screens/register_screen.dart';
+import 'package:viisoft/screens/welcome_screen.dart';
+import 'package:viisoft/widgets/my_button.dart';
+import 'package:viisoft/widgets/my_text_field.dart';
 
-class Home extends StatefulWidget {
-  static String namedRoute = '/homePage';
-  static String category = "All";
-
+class LoginScreen extends StatefulWidget {
+  static String namedRoute = '/loginScreen';
   @override
-  _HomeState createState() => _HomeState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _HomeState extends State<Home> {
-  List<CategoryModel> categories = new List<CategoryModel>();
-  List category1 = [];
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    category1 = categories.where((categoryItem) => categoryItem.label == "").toList();
-  }
+class _LoginScreenState extends State<LoginScreen> {
+  bool isHiddenPassword = true;
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    print(statusList.length);
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 100,
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection("Categories")
-                    .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Something went wrong');
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/authentication.png',
+                fit: BoxFit.contain,
+              ),
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'LibreBodoni',
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              MyTextField(
+                textEditingController: emailController,
+                labelText: 'Your Email',
+                textInputType: TextInputType.text,
+                validate: (value) {
+                  if (value.isEmpty) {
+                    return 'please enter valid User Name';
                   }
-
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
+                },
+                obscureText: false,
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              MyTextField(
+                textEditingController: passwordController,
+                labelText: 'Password',
+                textInputType: TextInputType.visiblePassword,
+                suffixIcon: IconButton(
+                  onPressed: _togglePasswordView,
+                  icon: Icon(
+                    isHiddenPassword ? Icons.visibility : Icons.visibility_off,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                validate: (value) {
+                  if (value.isEmpty) {
+                    return 'please enter valid User Name';
                   }
-                  // if (snapshot.data.docs.length == 0) {
-                  //   return CircularProgressIndicator();
-                  // }
-
-                  return ListView.builder(
-                    itemCount: snapshot.data.docs.length,
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            Home.category =
-                                snapshot.data.docs[index].data()['name'];
-                            print(Home.category);
-                            
-                          });
-                        },
-                        child: Stack(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(11),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    FontAwesomeIcons.accusoft,
-                                    color: Colors.blueGrey,
-                                    size: 20,
-                                  ),
-                                  SizedBox(height: 14),
-                                  Text(
-                                    snapshot.data.docs[index].data()['name'],
-                                    style: TextStyle(
-                                        color: Colors.blueGrey,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
+                },
+                obscureText: isHiddenPassword,
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              MyButton(
+                size: size,
+                title: 'Sign In',
+                onPress: () {
+                  Navigator.pushReplacementNamed(
+                      context, MainScreen.namedRoute);
+                },
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Don't have an Account ? ",
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return WelcomeScreen();
+                          },
                         ),
                       );
                     },
-                  );
-                },
-              ),
-            ),
-            StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection("AllProjects")
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Something went wrong');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
-
-                return Expanded(
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return Home.category ==
-                                  snapshot.data.docs[index]
-                                      .data()['category'] ||
-                              Home.category == "All"
-                          ? projectCard(
-                              img: snapshot.data.docs[index].data()['mainImg'],
-                              deveImg: snapshot.data.docs[index]
-                                  .data()['developerImg'],
-                              deveName: snapshot.data.docs[index]
-                                  .data()['developerName'],
-                              price: snapshot.data.docs[index].data()['price'],
-                              title: snapshot.data.docs[index].data()['title'],
-                              date: "21/2/1999",
-                              desc: snapshot.data.docs[index].data()['desc'],
-                              dislike:
-                                  snapshot.data.docs[index].data()['dislikes'],
-                              like: snapshot.data.docs[index].data()['likes'],
-                              listOfImages: snapshot.data.docs[index]
-                                  .data()['listOfImages'],
-                              toolused:
-                                  snapshot.data.docs[index].data()['toolUsed'],
-                              // snapshot.data.docs[index].data()['mainImg'],
-                              // snapshot.data.docs[index].data()['title'],
-                              // snapshot.data.docs[index].data()['price'],
-                              // snapshot.data.docs[index].data()['developerName'],
-                              // snapshot.data.docs[index].data()['developerImg'],
-                            )
-                          : SizedBox();
-                    },
-                    itemCount: snapshot.data.docs.length,
-                  ),
-                );
-              },
-            ),
-          ],
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context)
+                            .pushNamed(RegisterScreen.namedRoute);
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      isHiddenPassword = !isHiddenPassword;
+    });
   }
 }
